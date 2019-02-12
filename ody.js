12e5dc7e7b024bf62a4957a01d8b67b6a01135bd
Melkell <44038427+Melkell@users.noreply.github.com>
2019-02-12 16:15:52 +0100

    Add files via upload

---
+++ ody.js
@@ -0,0 +1,4 @@
+let nomFilm = "Nausicaa de la vallée du vent"
+let nomAuteur = "créé par Miyazaki du studio Ghibli"
+let an = "sorti en 1984"
+alert(`Mon film préféré est ${nomFilm},${nomAuteur}${an}`)
