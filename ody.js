ea504f2003c719560c7baef5b5477f5010758d6d
Melkell <44038427+Melkell@users.noreply.github.com>
2019-02-18 10:49:13 +0100

    Update ody.js

--- ody.js
+++ ody.js
@@ -1,4 +1,4 @@
-let nomFilm = "Nausicaa de la vallée du vent"
-let nomAuteur = "créé par Miyazaki du studio Ghibli"
-let an = "sorti en 1984"
-alert(`Mon film préféré est ${nomFilm},${nomAuteur}${an}`)
+let myMovie = "Nausicaa de la vallée du vent"
+let myAuthor = "créé par Miyazaki du studio Ghibli"
+let releaseDate = 1984
+alert(`Mon film préféré est ${myMovie},${myAuthor}sorti en ${releaseDate}.`)
